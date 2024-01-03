b7aaa3675c497bfe381c5aabc85a3f45b2d1335e
ashley phan <ashleyphan17@gmail.com>
2024-01-03 13:28:32 -0800

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splited = string.split("");
+    let reversed = splited.reverse("");
+    return reversed.join("");
 
 };
 
